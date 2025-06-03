47d23beebd91ed1e57f8d29d46ff9f287eae2a93
Viachaslau Rouski <vrovsky@gmail.com>
2025-06-03 12:03:08 +0100

    done-setup-drizzlekit

---
+++ lib/db/migrate.ts
@@ -0,0 +1,21 @@
+import { migrate } from "drizzle-orm/neon-http/migrator";
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("Database url is not set in .env");
+}
+
+async function runMigration() {
+  try {
+    const sql = neon(process.env.DATABASE_URL!);
+    const db = drizzle(sql);
+    await migrate(db, { migrationsFolder: "./drizzle" });
+    console.log("All migrations are successfully done");
+  } catch (error) {
+    console.log("Migration failed: ", error);
+    process.exit(1);
+  }
+}
+
+runMigration();
--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -1,4 +1,11 @@
-import { pgTable, text, uuid, integer, boolean } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  text,
+  uuid,
+  integer,
+  boolean,
+  timestamp,
+} from "drizzle-orm/pg-core";
 import { relations } from "drizzle-orm";
 
 export const filesTable = pgTable("files", {
@@ -17,4 +24,25 @@ export const filesTable = pgTable("files", {
   ///pwnership info
   userId: text("user_id").notNull(),
   parentId: uuid("parent_id").notNull(),
+
+  // file/folder flags
+  isFolder: boolean("is_folder").default(false).notNull(),
+  isStarred: boolean("is_starred").default(false).notNull(),
+  isTrash: boolean("is_trash").default(false).notNull(),
+
+  // timestamps
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });
+
+export const filesRelations = relations(filesTable, ({ one, many }) => ({
+  parent: one(filesTable, {
+    fields: [filesTable.parentId],
+    references: [filesTable.id],
+  }),
+
+  children: many(filesTable),
+}));
+
+export const File = typeof filesTable.$inferSelect;
+export const NewFile = typeof filesTable.$inferInsert;
